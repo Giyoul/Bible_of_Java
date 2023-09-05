e38abc6552b4f0d4d389976effb3332b51c25f7c
KimGiyoung <cla12093@gmail.com>
2023-09-06 00:19:28 +0900

    공부 내용

---
+++ Chapter1_자바를 시작하기 전에/1-3(자바로_프로그램_작성하기)/자바시작/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception { //class 안에 public static void main(String[] args)는 항상 적어주어야 한다
+        System.out.println("Hello, World!");
+    }
+}
