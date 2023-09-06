354574149cc67795782c0a9dcededeae234ca6c5
KimGiyoung <cla12093@gmail.com>
2023-09-06 21:21:46 +0900

    어디로 들어가지?

--- Chapter2_변수/변수/src/App.java
+++ Chapter2_변수/변수/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int year = 0;
+        int age;        // 초기값 없이 선언 가능
+        age = 12;
+
+        // 기본적인 문법은 c++과 유사
+        age+=1;
+        age++;
+        int tmp = year;
+        year = age;
+        age = tmp;
+        ++age;
+        
+
+        System.out.println("year = "+ year);
+        System.out.println("age = "+ age);
     }
 }
